df4da1ba77f2762de5d8f471e75b828914806ebb
Aeris <aeris.github@gmail.com>
2022-08-19 11:22:48 +0200

    Merged all options in a single file

--- QuickCompress.Core/Configuration/Options.cs
+++ QuickCompress.Core/Configuration/Options.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
 
 namespace QuickCompress.Core.Configuration;
 
@@ -7,9 +9,16 @@ public class Options : Context
 {
     private ThemeType _theme;
 
-    public Options(VideoCompressionOptions videoCompressionOptions)
+    public Options(string fFmpegPath, string fFprobePath, string tempPath, ObservableCollection<string> queuedVideos, ObservableCollection<string> queuedAudios)
     {
-        VideoCompressionOptions = videoCompressionOptions;
+
+        FFmpegPath = fFmpegPath;
+        FFprobePath = fFprobePath;
+        TempPath = tempPath;
+        QueuedVideos = queuedVideos;
+        QueuedAudios = queuedAudios;
+        QueuedVideos.CollectionChanged += (_, _) => SaveOptions();
+        QueuedAudios.CollectionChanged += (_, _) => SaveOptions();
     }
 
     /// <summary>
@@ -25,5 +34,32 @@ public class Options : Context
         }
     }
 
-    public VideoCompressionOptions VideoCompressionOptions { get; }
+    /// <summary>
+    /// The list of files still left to be compressed
+    /// </summary>
+    public ObservableCollection<string> QueuedVideos { get; }
+
+    [JsonIgnore]
+    public IEnumerable<FileInfo> QueuedVideosInfo => new List<FileInfo>(QueuedVideos.Select(x => new FileInfo(x)));
+
+    /// <summary>
+    /// The list of files still left to be compressed
+    /// </summary>
+    public ObservableCollection<string> QueuedAudios { get; }
+
+    [JsonIgnore]
+    public IEnumerable<FileInfo> QueuedAudiosInfo => new List<FileInfo>(QueuedAudios.Select(x => new FileInfo(x)));
+
+    /// <summary>
+    /// The path to ffmpeg.exe
+    /// </summary>
+    public string FFmpegPath { get; }
+    /// <summary>
+    /// The path to ffprobe.exe
+    /// </summary>
+    public string FFprobePath { get; }
+    /// <summary>
+    /// The path to the temporary folder
+    /// </summary>
+    public string TempPath { get; }
 }
